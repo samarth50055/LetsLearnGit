37e1100651a9e33f78e00ad91377d765f937ff5f
Samarth Birajdar <samarthbirajar2201@gmail.com>
2024-08-25 15:14:10 +0530

    first commit

---
+++ sum.java
@@ -0,0 +1,9 @@
+public class sum{
+    public static void main(String[] args) {
+    //sum in java
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    System.out.println("Sum of a and b is: " + sum);
+    }
+}
